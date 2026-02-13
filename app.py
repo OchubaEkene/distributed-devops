50d9c50ac725b800b14853cab112cd82515ef8c0
Ekene Ochuba <147551927+OchubaEkene@users.noreply.github.com>
2026-02-13 21:24:33 +0000

    Initial commit

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+import socket
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return f"Hostname: {socket.gethostname()}"
+
+app.run(host='0.0.0.0', port=3000)
