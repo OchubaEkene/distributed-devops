851269965d89cba3d9d8461b1acf473fdb03337b
Ekene Ochuba <147551927+OchubaEkene@users.noreply.github.com>
2026-02-13 22:28:10 +0000

    cicd live  commit

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return f"Hostname: {socket.gethostname()}"
+    return f"CI/CD LIVE: {socket.gethostname()}"
 
 app.run(host='0.0.0.0', port=3000)
